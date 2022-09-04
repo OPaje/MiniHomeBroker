d04e08e29ec87a994549692cf57f86928f23389a
OPaje <jecssilva@gmail.com>
2022-09-04 16:58:53 -0300

    criando cliente

---
+++ HomeBroker/src/homebroker/Cliente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package homebroker;
+
+/**
+ *
+ * @author jeanc
+ */
+public class Cliente {
+    int id;
+    String nome;
+    String cpf;
+    
+    
+}
