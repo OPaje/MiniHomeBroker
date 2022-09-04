648bb165ec6e6306aefc5cd2002a33a9b57c8a9b
OPaje <jecssilva@gmail.com>
2022-09-04 19:02:55 -0300

    criação das classes ativo e ordem

--- HomeBroker/src/homebroker/Menu.java
+++ HomeBroker/src/homebroker/Menu.java
@@ -15,7 +15,9 @@ public class Menu {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Testeeeee");
+        Cliente[] c = new Cliente[4];
+        
+        
     }
     
 }
