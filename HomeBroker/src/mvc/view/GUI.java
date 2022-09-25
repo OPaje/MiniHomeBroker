fca4f503e2a2750d12a7d52e080d37e50f1e6ef8
OPaje <jecssilva@gmail.com>
2022-09-25 14:12:06 -0300

    método criarCliente

--- HomeBroker/src/mvc/view/GUI.java
+++ HomeBroker/src/mvc/view/GUI.java
@@ -4,10 +4,42 @@
  */
 package mvc.view;
 
+import java.time.LocalDate;
+import javax.swing.JOptionPane;
+import mvc.model.Cliente;
+
 /**
  *
  * @author jeanc
  */
 public class GUI {
-    
+    public Cliente criarCliente(){
+        Cliente c = new Cliente();
+        String nome = JOptionPane.showInputDialog(null, "Informe o seu nome: ");
+        c.setNome(nome);
+        
+        String cpf = JOptionPane.showInputDialog(null, "Informe o seu CPF: ");
+        c.setCpf(cpf);
+        
+        String tipoUsuario = JOptionPane.showInputDialog(null, "Informe o tipo: ");
+        int tipo = Integer.parseInt(tipoUsuario);
+        c.setTipoUsuario(tipo);
+        
+        String login = JOptionPane.showInputDialog(null, "Informe o seu login: ");
+        c.setLogin(login);
+        
+        String senha = JOptionPane.showInputDialog(null, "Informe a sua sennha: ");
+        c.setSenha(senha);
+        
+        String endereco = JOptionPane.showInputDialog(null, "Informe o seu endereco: ");
+        c.setEndereco(endereco);
+        
+        String telefone = JOptionPane.showInputDialog(null, "Informe o seu telefone: ");
+        c.setTelefone(telefone);
+        
+        c.setDataCriacao(LocalDate.now());
+        c.setDataModificacao(LocalDate.now());
+        
+        return c;
+    }
 }
