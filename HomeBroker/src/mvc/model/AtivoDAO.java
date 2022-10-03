6a76c89f20dc54b41a4f3ad61294f8f6b508325a
OPaje <jecssilva@gmail.com>
2022-10-03 10:24:16 -0300

    CRUD Ativo

--- HomeBroker/src/mvc/model/AtivoDAO.java
+++ HomeBroker/src/mvc/model/AtivoDAO.java
@@ -4,10 +4,122 @@
  */
 package mvc.model;
 
+import java.time.LocalDate;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author jeanc
  */
 public class AtivoDAO {
+    private Ativo[] ativos = new Ativo[5];
+
+    public Ativo[] getAtivos() {
+        return ativos;
+    }
     
+    public AtivoDAO(){
+        Ativo a1 = new Ativo();
+        a1.setTicker("NTCO3");
+        a1.setNomeEmpresa("Grupo Natura");
+        a1.setPrecoInicial(10);
+        a1.setTotalAtivos(1000);
+        a1.setDataCriacao(LocalDate.now());
+        a1.setDataModficacao(LocalDate.now());
+        this.adicionaAtivo(a1);
+        
+        Ativo a2 = new Ativo();
+        a2.setTicker("PETR4");
+        a2.setNomeEmpresa("Petrobras");
+        a2.setPrecoInicial(10);
+        a2.setTotalAtivos(1000);
+        a2.setDataCriacao(LocalDate.now());
+        a2.setDataModficacao(LocalDate.now());
+        this.adicionaAtivo(a2);
+        
+        Ativo a3 = new Ativo();
+        a3.setTicker("ABEV3");
+        a3.setNomeEmpresa("Ambev");
+        a3.setPrecoInicial(10);
+        a3.setTotalAtivos(1000);
+        a3.setDataCriacao(LocalDate.now());
+        a3.setDataModficacao(LocalDate.now());
+        this.adicionaAtivo(a3);
+        
+    }
+    
+        public boolean adicionaAtivo(Ativo a) {
+        int proximaPosicaoLivre = this.proximaPosicaoLivre();
+        if (proximaPosicaoLivre != -1) {
+            ativos[proximaPosicaoLivre] = a;
+            return true;
+        } else {
+            return false;
+        }
+    }
+        
+        public Ativo buscaPorId(long id) {
+        for (Ativo a : ativos) {
+            if (a != null && a.getId() == id) {
+                return a;
+            }
+        }
+        return null;
+
+    }
+        
+        public Ativo buscaPorTicker(String ticker) {
+        for (Ativo a : ativos) {
+            if (a != null && a.getTicker().equals(ticker)) {
+                return a;
+            }
+        }
+        return null;
+
+    }
+        
+        public void mostrarTodos() {
+        boolean temAtivo = false;
+        for (Ativo a : ativos) {
+            if (a != null) {
+                JOptionPane.showMessageDialog(null, a);
+                temAtivo = true;
+            }
+        }
+        if (!temAtivo) {
+            System.out.println("Não existe ativo cadastrado");
+        }
+    }
+
+     public boolean alterarTicker(String ticker, String novoTicker){
+        Ativo a = this.buscaPorTicker(ticker);
+        if(a != null){
+            a.setTicker(novoTicker);
+            return true;
+        }else{
+            return false;
+        }
+    }
+     
+     public boolean remover(long id) {
+        for (int i = 0; i < ativos.length; i++) {
+            if (ativos[i] != null && ativos[i].getId() == id) {
+                ativos[i] = null;
+                return true;
+            }
+        }
+        return false;
+
+    }
+        
+        private int proximaPosicaoLivre() {
+        for (int i = 0; i < ativos.length; i++) {
+            if (ativos[i] == null) {
+                return i;
+            }
+
+        }
+        return -1;
+
+    }
 }
